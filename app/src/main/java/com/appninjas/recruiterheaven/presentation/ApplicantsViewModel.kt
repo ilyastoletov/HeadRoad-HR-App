2679b643d1274b00eafa842d341da55395514e26
iluckypunch <mr.artem.gribanov1977@gmail.com>
2023-05-23 23:29:26 +0300

    initial of presentation layer

---
+++ app/src/main/java/com/appninjas/recruiterheaven/presentation/ApplicantsViewModel.kt
@@ -0,0 +1,7 @@
+package com.appninjas.recruiterheaven.presentation
+
+import androidx.lifecycle.ViewModel
+
+class ApplicantsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/appninjas/recruiterheaven/presentation/MainActivity.kt
+++ app/src/main/java/com/appninjas/recruiterheaven/presentation/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.appninjas.recruiterheaven.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.appninjas.recruiterheaven.R
 import com.appninjas.recruiterheaven.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        bottomNavigationSetup()
+    }
+
+    private fun bottomNavigationSetup() {
+        val bottomNavigationView = binding.bottomNavigation
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 
 }
