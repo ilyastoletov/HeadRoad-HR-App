f98c4c66322bde1dfdd4f25d5523166d8fc11fb6
ilya <solomon-2021@list.ru>
2023-05-23 08:44:29 +0400

    Initial commit

---
+++ app/src/main/java/com/appninjas/recruiterheaven/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.appninjas.recruiterheaven.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.appninjas.recruiterheaven.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
